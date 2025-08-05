f765d1ee238c8b53958acf2ab352b3c02ab857df
Rajkumar Prajapati <ppraj916110@gmail.com>
2025-08-05 10:53:15 +0530

    Update script.js
    Signed-off-by: Rajkumar Prajapati <ppraj916110@gmail.com>

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.getElementById("menu-toggle");
+  const menu = document.getElementById("menu");
+
+  menuToggle.addEventListener("click", function () {
+    menu.classList.toggle("open");
+  });
+
+  AOS.init();
+});
 // JavaScript Enhancements for Trading Ek Mission Website
 
 document.addEventListener("DOMContentLoaded", function () {
